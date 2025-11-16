c14ba0aff55afa62f9b368d50f41dcefdf5577f4
Aryabell <aryabell.b@gmail.com>
2025-11-16 22:26:00 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/example/lab_week_10/FirstFragment.kt
+++ app/src/main/java/com/example/lab_week_10/FirstFragment.kt
@@ -10,30 +10,35 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class FirstFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         prepareViewModel()
     }
+
     private fun updateText(total: Int) {
         view?.findViewById<TextView>(R.id.text_total)?.text =
             getString(R.string.text_total, total)
     }
+
     private fun prepareViewModel(){
         val viewModel =
             ViewModelProvider(requireActivity()).get(TotalViewModel::class.java)
-        // Observe the LiveData object
+// Observe the LiveData object
         viewModel.total.observe(viewLifecycleOwner, {
-        // Whenever the value of the LiveData object changes
-        // the updateText() is called, with the new value as the parameter
+// Whenever the value of the LiveData object changes
+// the updateText() is called, with the new value as the parameter
             updateText(it)
         })
     }
--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -1,34 +1,78 @@
 package com.example.lab_week_10
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.lab_week_10.database.TotalDatabase
 import com.example.lab_week_10.viewmodels.TotalViewModel
+import com.example.lab_week_10.database.Total
 
 class MainActivity : AppCompatActivity() {
+    // Create database instance lazily
+    private val db by lazy { prepareDatabase() }
+
+    // ViewModel
     private val viewModel by lazy {
         ViewModelProvider(this)[TotalViewModel::class.java]
     }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         prepareViewModel()
+
+        // Initialize the value of the total from the database
+        initializeValueFromDatabase()
     }
+
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value!!))
+    }
+
+    // Create and build the TotalDatabase with the name 'total-database'
+    // allowMainThreadQueries() is used to allow queries to be run on the main thread (not recommended in production)
+    private fun prepareDatabase(): TotalDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            TotalDatabase::class.java, "total-database"
+        ).allowMainThreadQueries().build()
+    }
+
+    // Initialize the value of the total from the database
+    private fun initializeValueFromDatabase() {
+        val total = db.totalDao().getTotal(ID)
+        if (total.isEmpty()) {
+            db.totalDao().insert(Total(id = 1, total = 0))
+        } else {
+            viewModel.setTotal(total.first().total)
+        }
+    }
+
     private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
             getString(R.string.text_total, total)
     }
-    private fun prepareViewModel(){
+
+    private fun prepareViewModel() {
         // Observe the LiveData object
-        viewModel.total.observe(this, {
-            // Whenever the value of the LiveData object changes
-            // the updateText() is called, with the new value as the parameter
-            updateText(it)
-        })
+        viewModel.total.observe(this) { total ->
+            updateText(total)
+        }
+
         findViewById<Button>(R.id.button_increment).setOnClickListener {
             viewModel.incrementTotal()
         }
+    }
+
+    // The ID of the Total object in the database
+    // For simplicity, we only have one Total object in the database
+    // So the ID is always 1
+    companion object {
+        const val ID: Long = 1
     }
 }
